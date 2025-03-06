82dfc4be5eded78a64d068844758376454d1f0f1
jeongsoll <80992713+jeongsoll@users.noreply.github.com>
2025-03-06 16:07:54 +0900

    Initial commit

---
+++ Source/DeadHiDaylight/DeadHiDaylightGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeadHiDaylightGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeadHiDaylightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeadHiDaylightGameMode();
+};
+
+
+

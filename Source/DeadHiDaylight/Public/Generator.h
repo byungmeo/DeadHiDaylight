3c60b82a29b4075a8449f5661703686786df01fe
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-03-10 20:01:14 +0900

    발전기 게이지에 따른 애니메이션

---
+++ Source/DeadHiDaylight/Public/Generator.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Generator.generated.h"
+
+UCLASS()
+class DEADHIDAYLIGHT_API AGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGenerator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere, Category="Debugging")
+	float PowerGauge = 0.0f;
+};
---
+++ Source/DeadHiDaylight/Public/GeneratorAnim.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GeneratorAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADHIDAYLIGHT_API UGeneratorAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	TObjectPtr<class AGenerator> Generator = nullptr;
+	UPROPERTY(BlueprintReadOnly)
+	float PowerGauge = 0.0f;
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};

de0b1b2de6d2162afda90da55aba5877d33282bf
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-03-21 16:30:04 +0900

    병합하면서 누락된 부분 재추가

--- Source/DeadHiDaylight/Private/CamperAnimInstance.cpp
+++ Source/DeadHiDaylight/Private/CamperAnimInstance.cpp
@@ -52,6 +52,7 @@ void UCamperAnimInstance::AnimNotify_StartGen()
 void UCamperAnimInstance::AnimNotify_GenEnd()
 {
 	bStartRepair = false;
+	Montage_Stop(0.2f);
 }
 
 void UCamperAnimInstance::AnimNotify_StartSelfHealing()
@@ -62,6 +63,18 @@ void UCamperAnimInstance::AnimNotify_StartSelfHealing()
 void UCamperAnimInstance::AnimNotify_EndSelfHealing()
 {
 	bSelfHealing = false;
+	Montage_Stop(0.2f);
+}
+
+void UCamperAnimInstance::AnimNotify_StartUnLock()
+{
+	bUnLocking = true;
+}
+
+void UCamperAnimInstance::AnimNotify_EndUnLock()
+{
+	bUnLocking = false;
+	bUnLock = true;
 	Montage_Stop(0.2f);
 }
 
